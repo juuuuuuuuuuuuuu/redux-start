2defb0dde2e04fd93f8439c28888e5d892fe496e
이주영 <juu0124@wemakeprice.com>
2019-09-04 12:30:39 +0900

    (maaster): 카운터 예제 적용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,1 +1,23 @@
 // 리덕스와 연동된 컨테이너 컴포넌트 작성
+import React from 'react';
+import Counter from '../components/Counter';
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as counterActions from '../store/modules/counter';
+
+const CounterContainer = (props) => {
+  return (
+    <Counter
+      onIncrement={props.increment}
+      onDecrement={props.decrement}
+      number={props.number}
+    />
+  )
+}
+
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) => bindActionCreators(counterActions, dispatch)
+)(CounterContainer);
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,1 +1,9 @@
 // 모든 모듈들을 불러와서 합치는 작업이 이뤄짐
+// 여러개의 리듀서를 합쳐주는 역할
+
+import {combineReducers} from 'redux';
+import counter from './counter';
+
+export default combineReducers({
+  counter
+});
