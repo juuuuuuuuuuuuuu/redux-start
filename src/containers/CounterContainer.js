b1d187c1f3bb946c917bd0b304a465b85d860d05
이주영 <juu0124@wemakeprice.com>
2019-09-25 16:04:53 +0900

    (master):  useSelector, useDispatch 사용하여 예제 수정

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,13 +1,25 @@
 // 리덕스와 연동된 컨테이너 컴포넌트 작성
-import React from 'react';
+import React, { useCallback } from 'react';
 import Counter from '../components/Counter';
-import {connect} from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as counterActions from '../store/modules/counter_1';
+import { useSelector, useDispatch } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+// import * as counterActions from '../store/modules/counter_1';
 
-const CounterContainer = ({number, CounterActions}) => {
-  const handleIncrement = () => CounterActions.increment();
-  const handleDecrement = () => CounterActions.decrement();
+const CounterContainer = () => {
+  // const handleIncrement = () => CounterActions.increment();
+  // const handleDecrement = () => CounterActions.decrement();
+
+  // useSelector: mapStatetoProps 대신 사용, 두번째 파라미터는 배열로 어떤 값이 바뀌었을 때 selector를 재정의함,
+  // 두번째 파라미터 생략시 매번 렌더링 될 때마다 selector 함수로 새로 정의
+  // 두번째 파라미터 []로 하면 초기 세팅시에마 정의됨 (useCallback과 비슷)
+  // useSelector도 useMemo 사용하고 있음
+  const number = useSelector(state => state.counter.number, []);
+
+  // useDispatch: apDisptachtoProps
+  const dispatch = useDispatch();
+  const handleIncrement = useCallback(() => dispatch({type: 'counter/INCREMENT'}), [dispatch]);
+  const handleDecrement = useCallback(() => dispatch({type: 'counter/DECREMENT'}), [dispatch]);
+
   return (
     <Counter
       onIncrement={handleIncrement}
@@ -17,11 +29,13 @@ const CounterContainer = ({number, CounterActions}) => {
   )
 }
 
-export default connect(
-  (state) => ({
-    number: state.counter.number
-  }), 
-  (dispatch) => ({
-    CounterActions: bindActionCreators(counterActions, dispatch)
-  })
-)(CounterContainer);
+export default CounterContainer;
+
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number
+//   }), 
+//   (dispatch) => ({
+//     CounterActions: bindActionCreators(counterActions, dispatch)
+//   })
+// )(CounterContainer);
--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,28 +1,34 @@
 // 리덕스와 연동된 컨테이너 컴포넌트 작성
 
-import React from 'react';
+import React, { useCallback } from 'react';
 import Todos from '../components/Todos';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+// import { bindActionCreators } from 'redux';
 import * as todoActions from '../store/modules/todo_1';
 
 
-const TodosContainer = ({input, todos, TodoActions}) => {
+const TodosContainer = () => {
+  // useSelector
+  const { input, todos } = useSelector(state => ({ input: state.todo.input, todos: state.todo.todos}), []);
+
+  // useDispatch
+  const disptach = useDispatch();
+
   const handleChange = e => {
-    TodoActions.changeInput(e.target.value);
+    disptach(todoActions.changeInput(e.target.value))
   };
 
-  const handleInsert = () => { 
-    TodoActions.insert(input);
-    TodoActions.changeInput('');
+  const handleInsert = () => {
+    disptach(todoActions.insert(input))
+    disptach(todoActions.changeInput(''))
   }
 
   const handleToggle = (id) => {
-    TodoActions.toggle(id);
+    disptach(todoActions.toggle(id));
   }
 
   const handleRemove = (id) => {
-    TodoActions.remove(id);
+    disptach(todoActions.remove(id))
   }
 
 
@@ -38,15 +44,17 @@ const TodosContainer = ({input, todos, TodoActions}) => {
   );
 };
 
+export default TodosContainer;
+
 // mapStatetoProps, mapDispatchtoProps
-export default connect(
-  (state) => ({
-    input: state.todo.input,
-    todos: state.todo.todos,
-  }),
-  (dispatch) => ({
-    TodoActions: bindActionCreators(todoActions, dispatch)
-  })
-)(TodosContainer)
+// export default connect(
+//   (state) => ({
+//     input: state.todo.input,
+//     todos: state.todo.todos,
+//   }),
+//   (dispatch) => ({
+//     TodoActions: bindActionCreators(todoActions, dispatch)
+//   })
+// )(TodosContainer)
 
 
